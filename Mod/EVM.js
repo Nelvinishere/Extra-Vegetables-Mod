78d7e2d6d8d3d2828674c3a014a8a8ef96250e2e
Nelvinishere <161008445+Nelvinishere@users.noreply.github.com>
2024-06-13 09:43:11 +0000

    fixed bugs

--- Mod/EVM.js
+++ Mod/EVM.js
@@ -36,7 +36,7 @@ G.AddData({
 		new G.res({
 			name:'spinach',
 			desc:'after you discovered farming you can plant edible spinach',
-			turnToByContext:{'eat':{'health':0.03,'happiness':0.05},'decay'{'spoiled food':0.24}},
+			turnToByContext:{'eat':{'health':0.03,'happiness':0.05},'decay':{'spoiled food':0.24}},
 			icon:[0,2,'spicySheet'],
 			partOf:'food',
 			category:'food',
@@ -64,7 +64,7 @@ G.AddData({
 			//adding a new mode to artisans so they can make hot sauce from hot peppers
 		G.getDict('artisan').modes['hot sauce 1']={name:'Make hot sauce from hot peppers',desc:'Turn 3 [hot pepper]s and 3 [herb]s into 1 [hot sauce].',req:{'hot sauce preparing':true},use:{'knapped tools':1}};
 		G.getDict('artisan').modes['hot sauce 2']={name:'Make extra hot sauce', desc: 'turns 1 [cursed pepper], 3 [herb]s and [water] into hot sauce.',req:{'hot sauce preparing':true},use:{'knapped tools':1}};
-		G.getDict('farmer').modes['plant spinach']={name:'plant spinach', desc: 'turns spoiled food, fruit seeds and water into strong to the finish spinach',req:{'farming':true},use:{'knapped tools':1},
+		G.getDict('farmer').modes['plant spinach']={name:'plant spinach', desc: 'turns spoiled food, fruit seeds and water into strong to the finish spinach',req:{'farming':true},use:{'knapped tools':1}};
 		//adding a new effect to artisans that handles the actual hot sauce preparing and is only active when the unit has the mode "hot sauce"
 		G.getDict('artisan').effects.push,({type:'convert',from:{'hot pepper':3,'herb':3}, into:{'hot sauce':1},every:3,mode:'hot sauce 1'}),
 		G.getDict('artisan').effects.push,({type:'convert',from:{'cursed pepper':1,'herb':3,'water':1}, into:{'hot sauce':1},every:3,mode:'hot sauce 2'}),
